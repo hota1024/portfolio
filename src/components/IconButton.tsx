90a8da060b20bd9fe7e94f87f6decd115584d668
hota1024 <hotalog@hota1024.com>
2020-04-13 23:13:48 +0900

    Add src/components/IconButton.tsx

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react'
+
+export const IconButton: FC<{
+  size?: number
+}> = (props) => {
+  const style = {
+    width: props.size,
+    height: props.size,
+  }
+
+  return (
+    <>
+      <button style={style}>{props.children}</button>
+
+      <style jsx>{`
+        button {
+          border: none;
+          border-radius: 50%;
+          display: flex;
+          padding: 1em;
+          justify-content: center;
+          align-items: center;
+          cursor: pointer;
+          background: #f0f0f0;
+          color: #404040;
+          box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3);
+        }
+      `}</style>
+    </>
+  )
+}
+
+IconButton.defaultProps = {
+  size: 32,
+}
