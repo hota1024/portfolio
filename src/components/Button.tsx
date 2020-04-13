737c09c87c48af585bc8e180c4ffdf03d60db2d0
hota1024 <hotalog@hota1024.com>
2020-04-14 00:33:19 +0900

    Add src/components/Button.tsx

---
+++ src/components/Button.tsx
@@ -0,0 +1,55 @@
+import { FC } from 'react'
+
+export const Button: FC<{
+  background?: string
+  color?: string
+  href?: string
+  onClick?: () => void
+}> = (props) => {
+  const onClick = () => {
+    props.onClick()
+  }
+
+  return (
+    <>
+      {props.href ? (
+        <a className="button" onClick={onClick}>
+          {props.children}
+        </a>
+      ) : (
+        <button className="button" onClick={onClick}>
+          {props.children}
+        </button>
+      )}
+
+      <style jsx>{`
+        .button {
+          border: none;
+          border-radius: 3px;
+          background: ${props.background};
+          color: ${props.color};
+          min-width: 200px;
+          font-size: 1.05rem;
+          padding: 0.5em;
+          transition: 260ms;
+          cursor: pointer;
+          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        .button:hover {
+          box-shadow: 0 2px 16px rgba(0, 0, 0, 0.1);
+        }
+
+        .button:active {
+          box-shadow: none;
+        }
+      `}</style>
+    </>
+  )
+}
+
+Button.defaultProps = {
+  background: 'white',
+  color: '#404040',
+  onClick: () => void 0,
+}
