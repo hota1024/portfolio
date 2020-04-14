ae062ed40a8938065b53f5b24d36285b74f482a1
hota1024 <hotalog@hota1024.com>
2020-04-14 16:59:39 +0900

    Add src/pages/_app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from 'next/app'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, fas)
+
+const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+
+export default App
